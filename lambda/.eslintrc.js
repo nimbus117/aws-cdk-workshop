e175454e29e13438a4740d4ffec58e7eb2cbd5aa
nimbus117 <mail@nimbus117.co.uk>
2019-12-11 20:58:00 +0000

    complete workshop and add elsint

---
+++ lambda/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'script',
+  },
+  plugins: ['prettier', 'node'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'node/no-extraneous-require': [
+      'error',
+      {
+        allowModules: ['aws-sdk'],
+      },
+    ],
+  },
+};
--- lib/init-stack.ts
+++ lib/init-stack.ts
@@ -2,6 +2,7 @@ import cdk = require('@aws-cdk/core');
 import lambda = require('@aws-cdk/aws-lambda');
 import apigw = require('@aws-cdk/aws-apigateway');
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class InitStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -19,6 +20,11 @@ export class InitStack extends cdk.Stack {
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler,
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
     });
   }
 }
